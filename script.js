3332766c23e602503a007f2d5a86ca182d143577
MgeeN0 <125311334+MgeeN0@users.noreply.github.com>
2023-11-02 22:25:54 +0100

    Add hover effect, make custom grid creator

--- script.js
+++ script.js
@@ -4,6 +4,24 @@ for(let i=0;i<256;i++)
 {
     const square = document.createElement("div");
     square.classList.add('grid-element');
-    square.style.cssText = "border: 1px solid rgba(0, 0, 0, 0.8);width:60px;height:60px";
+    square.addEventListener("pointerover", function() {square.style.backgroundColor = 'red'});
     container.appendChild(square);
+}
+
+function squareAmount()
+{
+    let value = prompt("How many squares per side ?");
+    container.remove();
+    const body = document.querySelector("body")
+    const customContainer = document.createElement("div");
+    customContainer.classList.add('custom-grid');
+    customContainer.style.cssText = `display:grid;width:960px;height:960px;grid-template-columns: repeat(${value},1fr);grid-template-rows: auto;margin:0 auto;border: 4px solid rgba(0, 0, 0, 0.8)`
+    for(let i=0;i<value*value;i++)
+    {
+        const customSquare = document.createElement("div");
+        customSquare.addEventListener("pointerover", function() {customSquare.style.backgroundColor = 'red'});
+        customContainer.appendChild(customSquare);
+    }
+    body.appendChild(customContainer);
+
 }
